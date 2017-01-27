d8349fec745d406fd0432651029388b2d858d34f
ElizavetaBuraya <lizaburaya@gmail.com>
2017-01-28 01:20:05 +0300

    Revert "log"
    This reverts commit 77235c7a62f84069a9048671cf6518a20c630a3b.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -5,20 +5,20 @@ class GuessingGame {
     }
 
     setRange(min, max) {
-      this.min = min + 1;
+      this.min = min;
       this.max = max;
     }
 
     guess() {
-      return Math.ceil(this.min + ((this.max-this.min)/2));
+      return this.min + Math.ceil((this.max-this.min)/2);
     }
 
     lower() {
-      this.max = Math.ceil(this.min + (this.max - this.min)/2) - 1;
+      this.max = this.min + Math.ceil((this.max - this.min)/2) - 1;
     }
 
     greater() {
-      this.min = Math.ceil(this.min + (this.max - this.min)/2) + 1;
+      this.min = this.min + Math.ceil((this.max - this.min)/2) + 1;
     }
 }
 
